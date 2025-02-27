229dbab78dc8a4c29578aab56cd0f1d5ea343e3e
JUANCA99COL <jcamilocarorodriguez@gmail.com>
2025-02-28 11:43:05 +1300

    new update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
+import { eventListeners } from "@popperjs/core";
+
+const hamMenu = document.querySelector(".hamburger");
+const offScreenMenu = document.querySelector(".off-screen-menu");
+
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("is-active");
+  offScreenMenu.classList.toggle("active");
+});
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
